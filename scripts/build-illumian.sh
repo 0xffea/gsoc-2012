12c54023e3452e8a7dc889cb3b62fad5d8a6e95a
David Höppner <0xffea@gmail.com>
2012-06-18 18:03:52 -0700

    start illumian build script

---
+++ scripts/build-illumian.sh
@@ -0,0 +1,48 @@
+
+sudo apt-get install \
+data-docbook \
+developer-astdev \
+developer-build-make \
+developer-build-onbld \
+developer-gcc-3 \
+developer-java-jdk \
+developer-lexer-flex \
+developer-object-file \
+developer-parser-bison \
+developer-versioning-mercurial \
+library-glib2 \
+library-libxml2 \
+library-libxslt \
+library-nspr-header-nspr \
+library-perl-5-sun-solaris \
+library-perl-5-xml-parser \
+library-security-trousers \
+library-nspr \
+print-cups \
+print-filter-ghostscript \
+runtime-perl-512 \
+system-library-math-header-math \
+system-library-install \
+system-library-dbus \
+system-library-libdbus \
+system-library-libdbus-glib \
+system-library-mozilla-nss-header-nss \
+system-management-product-registry \
+system-management-snmp-net-snmp \
+text-gnu-gettext \
+library-python-2-python-extra-24 \
+web-server-apache-13 \
+system-library-mozilla-nss \
+developer-library-lint \
+system-header \
+developer-gnu-binutils \
+package-pkg \
+developer-gcc-44 \
+developer-macro-gnu-m4 \
+text-gnu-grep
+
+sudo apt-get install \
+developer-versioning-git
+
+
+# ctfmerge ldpath
